46ae9a99ea8d09576c84132da8cae7741838f4cf
Alessandro Sarretta <alessandro.sarretta@gmail.com>
2016-11-24 16:01:07 +0100

    Delete processed files, leave only original donwloaded zip files

---
+++ CopernicusEMS/EMSR190/copernicus_EMSR.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import zipfile
+import shutil
+import glob
+import shapefile
+
+###############################################################################
+
+folder_input_zip = "./files/zipinput/"
+folder_output_zip = "./files/zipoutput/"
+array_id = ["_01","_02","_03","_04","_05","_06","_07","_08","_09","_10","_11","_12","_13","_14","_15","_16","_17","_18","_19","_20","_21","_22","_23","_28","_29","_30","_31","_32"]
+folder_input_shp = folder_output_zip
+folder_output_shp = "./files/shapefiles/"
+array_elements = ['settlements_poly_grading', 'hydrography_line_grading', 'physiography_line', 'transportation_line_grading']
+folder_input_merge = folder_output_shp
+folder_output_merge = "./files/merged/"
+
+###############################################################################
+
+def extract_zipped_files (file_name):
+    #pass
+    print file_name
+    file_name_in=folder_input_zip+file_name
+    with zipfile.ZipFile(file_name_in, "r") as z:
+        z.extractall(folder_output_zip)
+
+def clean_folder ():
+    #pass
+    folder_del = folder_output_zip + "*.*"
+    files = sorted(glob.glob(folder_del))
+    for f in files:
+        os.remove(f)
+    for element in array_elements:
+        folder_del = folder_output_shp + element + "/*.*"
+        files = sorted(glob.glob(folder_del))
+        for f in files:
+            os.remove(f)
+    for element in array_elements:
+        folder_del = folder_output_merge + element + "_merged.*"
+        files = sorted(glob.glob(folder_del))
+        for f in files:
+            os.remove(f)
+
+
+clean_folder()
+#exit()
+
+for file in sorted(os.listdir(folder_input_zip)):
+    for id in array_id:
+        if id in file:
+            if file.endswith(".zip"):
+                extract_zipped_files(file)
+
+for file in sorted(os.listdir(folder_input_shp)):
+    for element in array_elements:
+        if element in file:
+            file_move_name_in = folder_input_shp + file
+            file_move_name_out = folder_output_shp + element + "/" + file
+            shutil.move(file_move_name_in,file_move_name_out)
+
+for category in array_elements:
+    folder_input_in = folder_input_merge + category + '/*.shp'
+    folder_input_out = folder_output_merge + category + '_merged.shp'
+    files = sorted(glob.glob(folder_input_in))
+    w = shapefile.Writer()
+    for f in files:
+        r = shapefile.Reader(f)
+        w._shapes.extend(r.shapes())
+        w.records.extend(r.records())
+        w.fields = list(r.fields)
+    w.save(folder_input_out)
+    folder_input_in = folder_input_merge + category + '/*.prj'
+    folder_input_out = folder_output_merge + category + '_merged.prj'
+    files = sorted(glob.glob(folder_input_in))
+    for f in files:
+        shutil.copy(f,folder_input_out)
+        break
+
+print "Finito !"
